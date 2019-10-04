b05fb0e03d9f22144177d26b45f083045fe08dbb
Jan Svacina <jan_svacina2@baylor.edu>
2019-10-04 15:25:30 -0500

    context module

---
+++ prophet-bounded-context/src/main/java/edu/baylor/ecs/prophet/bounded/context/App.java
@@ -0,0 +1,20 @@
+package edu.baylor.ecs.prophet.bounded.context;
+
+import edu.baylor.ecs.cloudhubs.database.EmbeddedNeo4j;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException {
+        EmbeddedNeo4j hello = new EmbeddedNeo4j();
+        hello.createDb();
+        hello.removeData();
+        hello.shutDown();
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ prophet-bounded-context/src/test/java/edu/baylor/ecs/prophet/bounded/context/AppTest.java
@@ -0,0 +1,20 @@
+package edu.baylor.ecs.prophet.bounded.context;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
