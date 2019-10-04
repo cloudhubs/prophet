1ef5ed07ec0bb4d8e2c1d1c295e1a2e3cd402f83
Jan Svacina <jan_svacina2@baylor.edu>
2019-10-04 15:24:40 -0500

    neo4j database - embedded database

---
+++ neo4j-database/src/test/java/edu/baylor/ecs/cloudhubs/database/AppTest.java
@@ -0,0 +1,20 @@
+package edu.baylor.ecs.cloudhubs.database;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
