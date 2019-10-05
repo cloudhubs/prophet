3e41fe926dc9a693800873a57cb536e98657caa1
Jan Svacina <jan_svacina2@baylor.edu>
2019-10-05 14:39:59 -0500

    database module facade

---
+++ neo4j-database/src/main/java/edu/baylor/ecs/cloudhubs/database/DatabaseFacade.java
@@ -0,0 +1,20 @@
+package edu.baylor.ecs.cloudhubs.database;
+
+import edu.baylor.ecs.cloudhubs.prophet.metamodel.Method;
+
+public class DatabaseFacade {
+
+    /**
+     * System1
+     *  -Module1
+     *      - EntityClass1
+     *      - EntityClass2
+     *  -Module2
+     *      -EntityClass3
+     * @return
+     */
+    public System getAllEntityClassesInSystem(String name){
+
+        return null;
+    }
+}
