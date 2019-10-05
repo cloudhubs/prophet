7a0801640c371a08b2ee8d9ae2520a3344de636a
Jan Svacina <jan_svacina2@baylor.edu>
2019-10-05 14:40:24 -0500

    bounded context databse api

--- prophet-bounded-context/src/main/java/edu/baylor/ecs/prophet/bounded/context/api/DatabseApi.java
+++ prophet-bounded-context/src/main/java/edu/baylor/ecs/prophet/bounded/context/api/DatabseApi.java
@@ -2,10 +2,13 @@ package edu.baylor.ecs.prophet.bounded.context.api;
 
 import java.util.List;
 
+/**
+ *
+ */
 public interface DatabseApi {
 
-    //get all context models from the system
-    List<String> getSysteContextmModels(String name);
+    // get all context models from the system
+    List<String> getAllEntityClassesInSystem(String name);
 
     // persist data to database
     String createBoundedContext(String system, String boundedContext);
