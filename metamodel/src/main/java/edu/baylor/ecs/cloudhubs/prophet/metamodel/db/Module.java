ebda1ac38ed543487a75943185f918a86d18b5ac
Jan Svacina <jan@svacina.net>
2019-10-07 15:20:12 -0500

    removing database model from metamodel

--- metamodel/src/main/java/edu/baylor/ecs/cloudhubs/prophet/metamodel/db/Module.java
+++
@@ -1,9 +0,0 @@
-package edu.baylor.ecs.cloudhubs.prophet.metamodel.db;
-
-public class Module {
-
-    static final String ip = "localhost:7070";
-    static final String name = "Module name";
-    static final String modulePath = "";
-
-}
