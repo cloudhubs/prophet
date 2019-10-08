974f19c5738a16ca89ca8fbe13698151875f05d9
Jan Svacina <jan@svacina.net>
2019-10-08 17:08:20 -0500

    tests using cdi and weld

--- neo4j-database/src/test/java/edu/baylor/ecs/cloudhubs/database/AppTest.java
+++ neo4j-database/src/test/java/edu/baylor/ecs/cloudhubs/database/AppTest.java
@@ -1,20 +1,39 @@
 package edu.baylor.ecs.cloudhubs.database;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import edu.baylor.ecs.cloudhubs.database.model.System;
+import edu.baylor.ecs.cloudhubs.prophet.metamodel.dto.parser.JPSystem;
+import org.jboss.weld.junit5.EnableWeld;
+import org.jboss.weld.junit5.WeldInitiator;
+import org.jboss.weld.junit5.WeldJunit5Extension;
+import org.jboss.weld.junit5.WeldSetup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.neo4j.graphdb.Node;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
  */
+@ExtendWith(WeldJunit5Extension.class)
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    @WeldSetup
+    public WeldInitiator weld = WeldInitiator.from(SystemRepository.class).build();
+
+
+    @DisplayName("Test MessageService.get()")
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void testGet(SystemRepository systemRepository) {
+        systemRepository.createDb();
+        systemRepository.createSystemModules(new JPSystem("a"));
+        JPSystem node = systemRepository.getByName("a");
+//        weld.select(SystemRepository.class).get().createSystemModules(new JPSystem());
+        assertEquals(node.getName(), "a");
+        systemRepository.shutDown();
     }
 }
