081ad224117ab1b21f66749c33cdd2c70caf40cc
Jan Svacina <jan@svacina.net>
2019-10-13 14:06:23 -0500

    junit tests

--- application/src/main/java/edu/baylor/ecs/cloudhubs/prophet/App.java
+++ application/src/main/java/edu/baylor/ecs/cloudhubs/prophet/App.java
@@ -2,10 +2,12 @@ package edu.baylor.ecs.cloudhubs.prophet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = "edu.baylor.ecs.cloudhubs.prophet")
+//@EnableNeo4jRepositories("movies.spring.data.neo4j.repositories")
 @RestController
 public class App {
 
@@ -18,7 +20,7 @@ public class App {
         this.embeddedDb = embeddedDb;
     }
 
-    @GetMapping("/")
+    @GetMapping("/system")
     public String home() {
         embeddedDb.registerDb();
         return myService.message();
--- graph-library/src/test/java/edu/baylor/ecs/cloudhubs/prophet/AppTest.java
+++ graph-library/src/test/java/edu/baylor/ecs/cloudhubs/prophet/AppTest.java
@@ -1,20 +1,43 @@
 package edu.baylor.ecs.cloudhubs.prophet;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.neo4j.test.extension.Inject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+
+@RunWith(JUnitPlatform.class)
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class AppTest {
+
+    @Autowired
+    private EmbeddedDb embeddedDb;
+
+    @Autowired
+    private MyService myService;
+
+
+    @Before
+    public void prepareTestDatabase() {
+        this.embeddedDb.registerDb();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldCreateNode() {
+        String message = this.myService.message();
+        assertEquals(message,"system name");
     }
 }
