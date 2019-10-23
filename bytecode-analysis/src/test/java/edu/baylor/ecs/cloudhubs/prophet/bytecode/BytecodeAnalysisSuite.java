2fa1e6b89ef2c8b182441ec334e9bb1b627419c6
Jan Svacina <jan@svacina.net>
2019-10-23 17:20:01 -0500

    tests for bytecode analysis

---
+++ bytecode-analysis/src/test/java/edu/baylor/ecs/cloudhubs/prophet/bytecode/BytecodeAnalysisSuite.java
@@ -0,0 +1,12 @@
+package edu.baylor.ecs.cloudhubs.prophet.bytecode;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ResourceServiceTest.class
+})
+public class BytecodeAnalysisSuite {
+
+}
