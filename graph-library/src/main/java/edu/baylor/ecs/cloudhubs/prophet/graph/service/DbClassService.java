1500297c098afb4cbfc5ffdd5746f1a63e566f80
Clinton Yeboah <clintonyeb@gmail.com>
2019-10-24 16:24:06 -0500

    Adding more tests for DbClassService API

--- graph-library/src/main/java/edu/baylor/ecs/cloudhubs/prophet/graph/service/DbClassService.java
+++ graph-library/src/main/java/edu/baylor/ecs/cloudhubs/prophet/graph/service/DbClassService.java
@@ -1,54 +1,57 @@
 package edu.baylor.ecs.cloudhubs.prophet.graph.service;
 
+import edu.baylor.ecs.cloudhubs.prophet.graph.exceptions.ConstraintViolationException;
+import edu.baylor.ecs.cloudhubs.prophet.graph.exceptions.EntityNotFoundException;
+import edu.baylor.ecs.cloudhubs.prophet.graph.model.DbClass;
+import edu.baylor.ecs.cloudhubs.prophet.graph.model.DbModule;
+import edu.baylor.ecs.cloudhubs.prophet.graph.repository.DbClassRepository;
+import edu.baylor.ecs.cloudhubs.prophet.metamodel.dto.parser.JPClassType;
+
+import java.util.Optional;
+
 public class DbClassService {
-//    private final DbClassRepository repository;
-//
-//    public DbClassService(DbClassRepository repository) {
-//        this.repository = repository;
-//    }
-//
-//    public void createClass(String className, JPClassType classType){
-//        DbClass dbClass = new DbClass();
-//        dbClass.setName(className);
-//
-//        if (validateClass(dbClass)) {
-//            repository.save(dbClass);
-//        } else {
-//            throw new IllegalStateException("Class name must be unique");
-//        }
-//    }
-//
-//    public Iterable<DbClass> getAllClasses() {
-//        return repository.findAll();
-//    }
-//
-//    public DbClass getClass(String name) {
-//        return repository.findByName(name);
-//    }
-//
-//    public DbClass getClass(long id) {
-//        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Class with id not found"));
-//    }
-//
-//    private boolean validateClass(DbClass dbClass) {
-//        return true;
-//    }
-//
-//    public void changeName(String oldName, String newName) {
-//        repository.setDbClassByName(oldName, newName);
-//    }
-//
-//    public void changeName(long id, String newName) {
-//        repository.setDbClassNameById(id, newName);
-//    }
-//
-//    public void deleteClass(String moduleName) {
-//        repository.deleteByName(moduleName);
-//    }
-//
-//    public void deleteClass(long id) {
-//        repository.deleteById(id);
-//    }
+    private final DbClassRepository repository;
 
+    public DbClassService(DbClassRepository repository) {
+        this.repository = repository;
+    }
 
+    public void createClass(String className, JPClassType classType){
+        DbClass dbClass = new DbClass();
+        dbClass.setName(className);
+
+        // check if no entity exists with name
+        Optional<DbClass> c = repository.findByName(dbClass.getName());
+        if(c.isPresent()) {
+            throw new ConstraintViolationException("DbClass with name already exists");
+        }
+
+        repository.save(dbClass);
+    }
+
+    public Iterable<DbClass> getAllClasses() {
+        return repository.findAll();
+    }
+
+    public DbClass getClass(String name) {
+        return repository.findByName(name).orElseThrow(() -> new EntityNotFoundException("Class with name not found"));
+    }
+
+    public void changeName(String oldName, String newName) {
+        // check if oldName exists
+        if(!repository.findByName(oldName).isPresent()) {
+            throw new ConstraintViolationException("DbModule with name does not exist");
+        }
+
+        // check if no system with new name exits
+        if(repository.findByName(newName).isPresent()) {
+            throw new ConstraintViolationException("DbModule with name already exists");
+        }
+
+        repository.setDbClassByName(oldName, newName);
+    }
+
+    public void deleteClass(String moduleName) {
+        repository.deleteByName(moduleName);
+    }
 }
---
+++ graph-library/src/test/java/edu/baylor/ecs/cloudhubs/prophet/graph/DbClassServiceTest.java
@@ -0,0 +1,28 @@
+package edu.baylor.ecs.cloudhubs.prophet.graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DbClassServiceTest {
+
+    @Test
+    public void createClass() {
+    }
+
+    @Test
+    public void getAllClasses() {
+    }
+
+    @Test
+    public void testGetClass() {
+    }
+
+    @Test
+    public void changeName() {
+    }
+
+    @Test
+    public void deleteClass() {
+    }
+}
