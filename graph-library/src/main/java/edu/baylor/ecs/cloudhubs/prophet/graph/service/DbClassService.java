129810b3ea5769d6ee1013fc566c1a6584d3c56d
Clinton Yeboah <clintonyeb@gmail.com>
2019-10-28 00:26:50 -0500

    Improves the class entity, later to be tested

--- graph-library/src/main/java/edu/baylor/ecs/cloudhubs/prophet/graph/service/DbClassService.java
+++ graph-library/src/main/java/edu/baylor/ecs/cloudhubs/prophet/graph/service/DbClassService.java
@@ -1,60 +1,88 @@
-//package edu.baylor.ecs.cloudhubs.prophet.graph.service;
-//
-//import edu.baylor.ecs.cloudhubs.prophet.graph.exceptions.ConstraintViolationException;
-//import edu.baylor.ecs.cloudhubs.prophet.graph.exceptions.EntityNotFoundException;
-//import edu.baylor.ecs.cloudhubs.prophet.graph.model.DbClass;
-//import edu.baylor.ecs.cloudhubs.prophet.graph.model.DbModule;
-//import edu.baylor.ecs.cloudhubs.prophet.graph.repository.DbClassRepository;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class DbClassService {
-//    private final DbClassRepository repository;
-//    private final DbModuleService moduleService;
-//
-//    public DbClassService(DbClassRepository repository, DbModuleService moduleService) {
-//        this.repository = repository;
-//        this.moduleService = moduleService;
-//    }
-//
-//    public void createByName(String moduleName, String className){
-//        DbClass dbClass = new DbClass();
-//        dbClass.setName(className);
-//
-//        // get module
-////        DbModule module = moduleService.getModule(moduleName);
-//
-//        // check if no entity exists with name and systemName
-//        if(module.getClasses().contains(dbClass)) {
-//            throw new ConstraintViolationException("Module has class with same name");
-//        }
-//
-//        repository.save(dbClass);
-//    }
-//
-//    public Iterable<DbClass> getAll() {
-//        return repository.findAll();
-//    }
-//
-//    public DbClass getClass(String name) {
-//        return repository.findByName(name).orElseThrow(() -> new EntityNotFoundException("Class with name not found"));
-//    }
-//
-//    public void changeName(String oldName, String newName) {
-//        // check if oldName exists
-//        if(!repository.findByName(oldName).isPresent()) {
-//            throw new ConstraintViolationException("DbModule with name does not exist");
-//        }
-//
-//        // check if no system with new name exits
-//        if(repository.findByName(newName).isPresent()) {
-//            throw new ConstraintViolationException("DbModule with name already exists");
-//        }
-//
-//        repository.setDbClassByName(oldName, newName);
-//    }
-//
-//    public void deleteClass(String moduleName) {
-//        repository.deleteByName(moduleName);
-//    }
-//}
+package edu.baylor.ecs.cloudhubs.prophet.graph.service;
+
+import edu.baylor.ecs.cloudhubs.prophet.graph.exceptions.ConstraintViolationException;
+import edu.baylor.ecs.cloudhubs.prophet.graph.exceptions.EntityNotFoundException;
+import edu.baylor.ecs.cloudhubs.prophet.graph.model.DbClass;
+import edu.baylor.ecs.cloudhubs.prophet.graph.model.DbModule;
+import edu.baylor.ecs.cloudhubs.prophet.graph.model.relationship.HasAClassRel;
+import edu.baylor.ecs.cloudhubs.prophet.graph.repository.ClassRelRepository;
+import edu.baylor.ecs.cloudhubs.prophet.graph.repository.DbClassRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class DbClassService {
+    private final DbClassRepository repository;
+    private final DbModuleService moduleService;
+    private final ClassRelRepository classRelRepository;
+
+    public DbClassService(DbClassRepository repository, DbModuleService moduleService, ClassRelRepository classRelRepository) {
+        this.repository = repository;
+        this.moduleService = moduleService;
+        this.classRelRepository = classRelRepository;
+    }
+
+    public DbClass createByName(String systemName, String moduleName, String className) {
+        // get system
+        DbModule module = moduleService.getModule(systemName, moduleName);
+
+        // check if class already exists exists
+        if (module.getClasses().stream().anyMatch(clazz -> clazz.getName().equals(className))) {
+            throw new ConstraintViolationException("Module has class with same name");
+        }
+
+        // create module
+        DbClass dbClass = new DbClass();
+        dbClass.setName(className);
+
+        HasAClassRel classRel = new HasAClassRel();
+        classRel.setModule(module);
+        classRel.setClazz(dbClass);
+        classRel.setSystem(module.getSystemRel().getSystem());
+        dbClass.setModuleRel(classRel);
+
+        moduleService.addClassToModule(module, dbClass);
+
+        // save relationship and return the module
+        return classRelRepository.save(classRel).getClazz();
+    }
+
+    public Iterable<DbClass> getAll() {
+        return repository.findAll();
+    }
+
+    public DbClass getClass(String systemName, String moduleName, String name) {
+        DbModule module = moduleService.getModule(systemName, moduleName);
+        return module.getClasses().stream().filter(dbClass -> dbClass.getName().equals(name)).findFirst()
+                .orElseThrow(() -> new EntityNotFoundException("System-Module-Class pair not found"));
+    }
+
+    public DbClass changeName(String systemName, String moduleName, String oldName, String newName) {
+        // get system
+        // will throw an error if module does not exist
+        DbModule module = moduleService.getModule(systemName, moduleName);
+        DbClass dbClass = findDbClassFromModule(module.getClasses(), oldName);
+
+        if (dbClass == null) {
+            throw new EntityNotFoundException("Relationship among entities not found");
+        } else {
+            dbClass.setName(newName);
+        }
+
+        // save relationship and return the class
+        return repository.save(dbClass);
+    }
+
+    public void deleteClass(String systemName, String moduleName, String className) {
+        repository.delete(getClass(systemName, moduleName, className));
+    }
+
+    private DbClass findDbClassFromModule(Set<DbClass> dbClasses, String className) {
+        for (DbClass c : dbClasses) {
+            if (c.getName().equals(className))
+                return c;
+        }
+        return null;
+    }
+}
