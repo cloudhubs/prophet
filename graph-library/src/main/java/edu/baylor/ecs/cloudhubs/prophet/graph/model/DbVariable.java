489ef6d86c9a66cc2cb617a25aa75a4ca4d21dd3
Jan Svacina <jan@svacina.net>
2019-11-05 15:54:31 -0600

    class and variable rel

--- graph-library/src/main/java/edu/baylor/ecs/cloudhubs/prophet/graph/model/DbVariable.java
+++ graph-library/src/main/java/edu/baylor/ecs/cloudhubs/prophet/graph/model/DbVariable.java
@@ -7,6 +7,8 @@ import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
+import java.util.List;
+
 @NodeEntity
 @Data
 @NoArgsConstructor
@@ -18,11 +20,14 @@ public class DbVariable {
 
     private String name;
 
-    @Relationship(value = "HAS_A_CLASS")
+    @Relationship(value = "HAS_A_CLASS", direction = Relationship.INCOMING)
     private DbClass dbClass;
 
     @Relationship(value = "HAS_A_METHOD")
     private DbMethod method;
+
+    @Relationship(value = "HAS_AN_ANNOTATION")
+    private List<DbAnnotation> annotations;
 
 
 }
