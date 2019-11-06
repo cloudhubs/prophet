d95b7f106e354f29cde2fd876befad03b93ee409
Jan Svacina <jan@svacina.net>
2019-11-06 13:55:07 -0600

    jparser service

---
+++ application/src/main/java/edu/baylor/ecs/cloudhubs/prophet/application/services/JParserService.java
@@ -0,0 +1,10 @@
+package edu.baylor.ecs.cloudhubs.prophet.application.services;
+
+import edu.baylor.ecs.cloudhubs.prophet.metamodel.service.FacadeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JParserService extends FacadeService {
+
+    //Todo: call Jparser library and convert data and insert to db via FacadeService
+}
---
+++ metamodel/src/main/java/edu/baylor/ecs/cloudhubs/prophet/metamodel/service/FacadeService.java
@@ -0,0 +1,9 @@
+package edu.baylor.ecs.cloudhubs.prophet.metamodel.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public abstract class FacadeService {
+
+
+}
