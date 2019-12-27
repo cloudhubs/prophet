fe09c3b899b1a07c13de937b4f4e79b5542c9375
Jan Svacina <jan@svacina.net>
2019-12-27 23:42:25 +0100

    ctrl for prophet request

--- src/main/java/edu/baylor/ecs/cloudhubs/prophet/controller/JParserController.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophet/controller/JParserController.java
@@ -1,8 +1,11 @@
 package edu.baylor.ecs.cloudhubs.prophet.controller;
 
+import edu.baylor.ecs.cloudhubs.prophet.model.ProphetRequest;
 import edu.baylor.ecs.cloudhubs.prophet.services.JParserService;
 import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.SystemContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +14,9 @@ public class JParserController {
     @Autowired
     private JParserService jParserService;
 
-    public SystemContext parseApplication(String path) {
-        return jParserService.getSystemContextFromFile(path);
+    @PostMapping("/")
+    public SystemContext parseApplication(@RequestBody ProphetRequest request) {
+        return jParserService.getSystemContextFromFile(request.getUrl());
     }
 
 }
---
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophet/model/ProphetRequest.java
@@ -0,0 +1,16 @@
+package edu.baylor.ecs.cloudhubs.prophet.model;
+
+public class ProphetRequest {
+
+    private String url;
+
+    public ProphetRequest(){}
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
