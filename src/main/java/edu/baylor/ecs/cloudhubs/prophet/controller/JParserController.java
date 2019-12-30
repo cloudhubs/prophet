02c8e0ce906a23fbf7c2fef187f943ae58b30529
Jan Svacina <jan@svacina.net>
2019-12-30 23:19:24 +0100

    analyze ctrl for prophet app

--- src/main/java/edu/baylor/ecs/cloudhubs/prophet/controller/JParserController.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/prophet/controller/JParserController.java
@@ -1,12 +1,15 @@
 package edu.baylor.ecs.cloudhubs.prophet.controller;
 
-import edu.baylor.ecs.cloudhubs.prophet.model.ProphetRequest;
 import edu.baylor.ecs.cloudhubs.prophet.services.JParserService;
-import edu.baylor.ecs.cloudhubs.prophetdto.systemcontext.SystemContext;
+import edu.baylor.ecs.cloudhubs.prophetdto.app.ProphetRequest;
+import edu.baylor.ecs.cloudhubs.prophetdto.app.ProphetResponse;
+import edu.baylor.ecs.cloudhubs.prophetutils.ProphetUtilsFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class JParserController {
@@ -14,9 +17,16 @@ public class JParserController {
     @Autowired
     private JParserService jParserService;
 
-    @PostMapping("/")
-    public SystemContext parseApplication(@RequestBody ProphetRequest request) {
-        return jParserService.getSystemContextFromFile(request.getUrl());
+//    @PostMapping("/")
+//    public SystemContext parseApplication(@RequestBody ProphetRequest request) {
+//        return jParserService.getSystemContextFromFile(request.getUrl());
+//    }
+
+    @PostMapping("/analyze")
+    public ProphetResponse parseApplication(@RequestBody ProphetRequest pr) throws IOException {
+//        ObjectMapper mapper = new ObjectMapper();
+//        ProphetRequest pr = mapper.readValue(req.toString(), ProphetRequest.class);
+        return ProphetUtilsFacade.getProphetResponse(pr.getUrl());
     }
 
 }
